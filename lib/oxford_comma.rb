ead361e1ec66bace8d7aaef1f0c58f354a636222
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-12 01:42:27 +0900

    Completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.length == 1
+  "#{array[0]}"
+elsif array.length == 2
+  "#{array.first} and #{array.last}"
+elsif array.length == 3
+  "#{array[0]}, #{array[1]}, and #{array[2]}"
+else
+  "#{array[0...-1].join(", ")}, and #{array[-1]}"
+end
 end
